4b6fdf39e19be8c59b3a8066e91d9c06bd24ab90
KingCop <copterlovenfsmw2012@gmail.com>
2018-01-24 15:49:46 +0700

    Lab5

---
+++ src/main/java/rmuti/LABLAB05/ArrayListPQ.java
@@ -0,0 +1,46 @@
+package rmuti.LABLAB05;
+
+import java.util.ArrayList;
+
+public class ArrayListPQ {
+	private ArrayList lst;
+    
+    public ArrayListPQ() {
+   	 lst = new ArrayList();
+    }
+
+    public void enqueue(Object e) {
+   	 lst.add(lst.size(),e);
+    }
+
+    public Object dequeue() {
+   	 int maxIndex = maxIndex();
+   	 Object max0bj = lst.get(maxIndex);
+   	 lst.remove(maxIndex);
+   	 return max0bj;
+    }
+    
+    public Object peek() {
+   	 return lst.get(maxIndex());
+    }
+
+    private int maxIndex() {
+   	 int max = 0;
+   	 for(int i=0; i < lst.size(); i++) {
+   		 Comparable d = (Comparable) lst.get(i);
+   		 Object maxObj = lst.get(max);
+   		 if(d.compareTo(maxObj)>0) {
+   	        max=i;
+   		 }
+        }
+        return max;
+    }
+//Override  
+    public String toString() {
+   	 String str = "[";
+   	 for (int i = 0; i<lst.size(); i++) {
+   		 str += lst.get(i)+" , ";
+        }
+        return str+"]";     
+    }
+}
---
+++ src/main/java/rmuti/LABLAB05/Employee.java
@@ -0,0 +1,27 @@
+package rmuti.LABLAB05;
+
+public class Employee implements Comparable{
+    private String empId;
+    private String name;
+    private int salary;
+   
+    public Employee(String emId, String name, int salary) {
+    	this.empId = empId;
+    	this.name = name;
+    	this.salary = salary;
+    }
+    
+    
+    public String toString() {
+    	return "{empId = "+empId+",name= "+name+", salary= "+salary+")";
+    		}
+    public int compareTo(Object o) {
+    	Employee otherEmp = (Employee) o;
+    	if (this.salary > otherEmp.salary) {
+    		return 1;
+    	} else if (this.salary < otherEmp.salary) {
+            return -1;     
+    	}
+        return 0;
+    }
+}
